d733f3551af13d432db89d23b4123a436039921a
Sharmahaihum <108683688+Sharmahaihum@users.noreply.github.com>
2022-12-20 23:13:48 +0530

    Add files via upload

---
+++ Linear Search.py
@@ -0,0 +1,18 @@
+def linear_search(data, target):
+    for i in range(len(data)):
+        if data[i] == target:
+            print("Data found at:",i)
+    
+
+def main():
+    data = []
+    limit=int(input("Enter size of list"))
+    for i in range (limit):
+        a=input()
+        data.append(a)
+    target = input("Enter a target value: ")
+    index = linear_search(data, target)
+        
+
+if __name__ == '__main__':
+    main()
